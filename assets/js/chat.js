85ceab147e843464ba5b7a9bc0a1a6a679ab42c3
František Žiačik <ziacik@gmail.com>
2014-04-16 00:41:24 +0200

    Real-time messaging.

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,4 +1,44 @@
 $(document).ready(function() {
+	console.log('Qq');
+
+	var SailsCollection = Backbone.Collection.extend({
+	    sailsCollection: "",
+	    socket: null,
+	    sync: function(method, model, options){
+	    	console.log('sync');
+	        var where = {};
+	        if (options.where) {
+	            where = {
+	                where: options.where
+	            };
+	        }      
+	        if(typeof this.sailsCollection === "string" && this.sailsCollection !== "") {
+	        	console.log('gonna connect');
+	            this.socket = io.connect();
+	            this.socket.on("connect", _.bind(function(){
+		        	console.log('connected');
+	                this.socket.request("/" + this.sailsCollection, where, _.bind(function(users){
+			        	console.log('users');
+	                    this.set(users);
+	                }, this));
+	     
+	                this.socket.on("message", _.bind(function(msg){
+			        	console.log('message');
+	                    var m = msg.verb;
+	                    if (m === "create") {
+	                        this.add(msg.data);
+	                    } else if (m === "update") {
+	                        this.get(msg.data.id).set(msg.data);
+	                    } else if (m === "destroy") {
+	                        this.remove(this.get(msg.data.id));
+	                    }
+	                }, this));
+	            }, this));
+	        } else {
+	            console.log("Error: Cannot retrieve models because property 'sailsCollection' not set on the collection");
+	        }
+	    }
+	});	
 
 	var UserModel = Backbone.Model.extend({
 		urlRoot : '/user',
@@ -13,15 +53,20 @@ $(document).ready(function() {
 		urlRoot : '/message',
 	});
 
-	var MessageCollection = Backbone.Collection.extend({
-		url : '/message',
-		model : MessageModel
+//	var MessageCollection = Backbone.Collection.extend({
+//		url : '/message',
+//		model : MessageModel
+//	});
+	
+	var MessageCollection = SailsCollection.extend({
+	    sailsCollection: 'message',
+	    model: MessageModel
 	});
 
 	_.templateSettings = {
 		interpolate : /\{\{(.+?)\}\}/g
 	};
-
+	
 	var UsersView = Backbone.View.extend({
 		el : '#usersContainer',
 		initialize : function() {
@@ -68,9 +113,10 @@ $(document).ready(function() {
 
 	messages.fetch();
 
-	/*
-	 * $("#postMessageButton").click(function(){ var messageText =
-	 * $("#message").val(); messages.create({message: messageText}, {wait:
-	 * true}); $("#message").val(""); });
-	 */
+	
+	$("#sendButton").click(function() { 
+		var messageText = $("#message").val();
+		messages.create({sender:'kolik', content: messageText}, {wait: true}); 
+		$("#message").val(""); 
+	});	 
 });
